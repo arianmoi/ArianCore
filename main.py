99c3e2c04032232f8c1675667b1eec371c24fab4
Arian <t-amoinian@microsoft.com>
2021-09-12 19:21:58 +0300

    chenge func name

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def just_changed_it():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    just_changed_it()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
